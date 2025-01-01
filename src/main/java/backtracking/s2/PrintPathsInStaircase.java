120331fa0017a68dcd1f1d6f872c9aff91298c4d
Ishan Aggarwal <ishan.aggarwal.1423@gmail.com>
2025-01-02 00:11:17 +0530

    Q2. Print Paths In Staircase (add problem statement)

--- src/main/java/backtracking/s2/PrintPathsInStaircase.java
+++ src/main/java/backtracking/s2/PrintPathsInStaircase.java
@@ -2,6 +2,53 @@ package backtracking.s2;
 
 import java.util.ArrayList;
 
+/**
+ * Q2. Print paths in Staircase
+ * Problem Description
+ * You are climbing a staircase and it takes A steps to reach the top.
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ * You need to return all the distinct ways to climb to the top in lexicographical order.
+ * <p>
+ * Problem Constraints
+ * <p>
+ * 2 <= A <= 25
+ * <p>
+ * Input Format
+ * <p>
+ * The first and the only argument contains an integer A, the number of steps.
+ * <p>
+ * Output Format
+ * <p>
+ * Return an 2-D Integer Array, which denotes all the unique valid paths to reach the top.
+ * <p>
+ * Example Input
+ * <p>
+ * Input 1:
+ * <p>
+ * A = 2
+ * Input 2:
+ * <p>
+ * A = 3
+ * Example Output
+ * <p>
+ * Output 1:
+ * <p>
+ * [ [1, 1], [2] ]
+ * Output 2:
+ * <p>
+ * [ [1, 1, 1], [1, 2], [2, 1] ]
+ * <p>
+ * Example Explanation
+ * <p>
+ * Explanation 1:
+ * <p>
+ * Distinct ways to reach top: 1 + 1, 2.
+ * Explanation 2:
+ * <p>
+ * Distinct ways to reach top: 1 + 1 + 1, 1 + 2, 2 + 1.
+ */
+
+
 public class PrintPathsInStaircase {
 
     public static void main(String[] args) {
